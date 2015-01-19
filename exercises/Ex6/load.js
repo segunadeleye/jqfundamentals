42a5ff7575c80af0159a36903df1c6f74bc1a53f
Oluwasegun Adeleye <oluwasegun@dealdey.com>
2015-01-19 19:10:47 +0100

    Exercise 6.

---
+++ exercises/Ex6/load.js
@@ -0,0 +1,33 @@
+
+function Load() {
+
+}
+
+Load.prototype.loadPost = function() {
+
+  var $headers = $('#blog h3');
+
+  $headers.each(function() {
+    var $div = $('<div />');
+    $(this).data('postDiv', $div);
+  });
+
+  $headers.click(function(event) {
+    event.preventDefault();
+    var $h3 = $(this);
+    var $postId = $h3.index('h3');
+    var $li = $h3.parent('li');
+    $li
+      .append(
+        $h3
+          .data('postDiv')
+            .load('blog.html div:eq(' + $postId + ')'));
+  });
+}
+
+$(document).ready(function() {
+
+  var load = new Load();
+  load.loadPost();
+
+});
---
+++ exercises/Ex6/specials.js
@@ -0,0 +1,63 @@
+
+function Specials() {
+}
+
+Specials.prototype.getSpecial = function() {
+
+  var that = this;
+  
+  var $specials = $('#specials');
+  var $div = $('<div />');
+
+  $specials
+    .append($div)
+    .find('li.buttons')
+      .remove();
+
+  $specials
+    .find('select')
+      .change(function() {
+        
+        var $option = $(this);
+        
+        $.getJSON('specials.json', function(data) {
+
+          var day = $option.val();
+          that.displaySpecial(data, day);
+          
+        });
+      });
+}
+
+Specials.prototype.displaySpecial = function(response, value) {
+
+  if (value) {
+    
+    $div.empty();
+
+    var $title = $('<h2 />');
+    var $text = $('<p />');
+    var $image = $('<img />');
+
+    $div.append($title, $text, $image);
+
+    var special = response[value];
+    
+    $title
+      .css('color', special.color)
+      .html(special.title);
+
+    $text.html(special.text);
+    $image.attr('src', special.image);
+
+  } else {
+    $div.empty();
+  }
+};
+
+$(document).ready(function() {
+
+  var specials = new Specials();
+  specials.getSpecial();
+
+});
