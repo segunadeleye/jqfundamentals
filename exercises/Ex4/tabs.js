23e186e2bfdcf689ac994248f308e08938d000d5
Oluwasegun Adeleye <oluwasegun@dealdey.com>
2015-01-22 14:00:29 +0100

    Add tabbed navigation.

--- exercises/Ex4/tabs.js
+++ exercises/Ex4/tabs.js
@@ -3,61 +3,45 @@ function Tabs() {
 }
 
 Tabs.prototype.showContent = function() {
-  
-  // 1. Hide all of the modules.
     
-    var $modules = $('.module')
-    $modules.hide();
+  var $modules = $('.module');
+  $modules.hide();
 
-  // 2. Create an unordered list element before the first module.
+  var $list = $('<ul />');
+  $list.insertBefore($modules.first());
 
-    var $list = $('<ul></ul>');
-    $list.insertBefore($modules.first());
+  $modules.each(function() {
+    var h2Text = $(this).children('h2').text();
+    var $listItem = $('<li />').text(h2Text);
 
-  // 3. Iterate over the modules using $.fn.each. For each module, use the text of the h2 element as the text for a list item that you add to the unordered list element.
-
-    $modules.each(function() {
-      var h2Text = $(this).children('h2').text();
-      var $listItem = $('<li />').text(h2Text);
-
-      $listItem
-        .css('cursor', 'pointer')
-        .appendTo($list);
-    });
-
-  // 4. Bind a click event to the list item that:
-
-      // a. Shows the related module, and hides any other modules
-
-      // b. Adds a class of "current" to the clicked list item
-
-      // c. Removes the class "current" from the other list item
+    $listItem
+      .css('cursor', 'pointer')
+      .appendTo($list);
+  });
       
-        $listItem = $list.children();
-        $listItem.on('click', function() {
-          var listItemIndex = $(this).index();
+  $listItem = $list.children();
+  $listItem.on('click', function() {
+    var listItemIndex = $(this).index();
 
-          $(this)
-            .addClass('current')
-            .siblings('li')
-            .removeClass('current');
+    $(this)
+      .addClass('current')
+      .siblings('li')
+      .removeClass('current');
 
-          $modules
-            .eq(listItemIndex)
-            .show()
-            .siblings('.module')
-            .hide();
-        });
+    $modules
+      .eq(listItemIndex)
+      .show()
+      .siblings('.module')
+      .hide();
+  });
 
-  // 5. Finally, show the first tab.
-      
-      $listItem
-        .eq(0)
-        .addClass('current');
+  $listItem
+    .eq(0)
+    .addClass('current');
 
-      $modules
-        .eq(0)
-        .show();
+  $modules
+    .eq(0)
+    .show();
 }
   
 
