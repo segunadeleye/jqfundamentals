52d1ce00872058c659037a721ed6e82152b9f2af
Oluwasegun Adeleye <oluwasegun@dealdey.com>
2015-01-22 13:59:50 +0100

    Create an input hint.

--- exercises/Ex4/inputHint.js
+++ exercises/Ex4/inputHint.js
@@ -4,40 +4,25 @@ function InputHint() {
 
 InputHint.prototype.createHint = function() {
   
-  // 1. Set the value of the search input to the text of the label element
+  var $searchInput = $('input[name=q]');
 
-    var $searchInput = $('input[name=q]');
+  var labelText = $('label').text();
 
-    var labelText = $('label').text();
+  $searchInput.val(labelText);
 
-    $searchInput.val(labelText);
+  $searchInput.addClass('hint');
 
+  $('label').remove();
 
-  // 2. Add a class of "hint" to the search input
+  $searchInput.on('focus', function() {
+    $(this).removeClass('hint').val('');
+  });
 
-    $searchInput.addClass('hint');
-
-
-  // 3. Remove the label element
-
-    $('label').remove();
-
-
-  // 4. Bind a focus event to the search input that removes the hint text and the "hint" class
-
-    $searchInput.on('focus', function() {
-      $(this).removeClass('hint').val('');
-    });
-
-
-  // 5. Bind a blur event to the search input that restores the hint text and "hint" class if no search text was entered
-
-    $searchInput.on('blur', function() {
-      if ($.trim($(this).val() === '')) {
-        $(this).addClass('hint').val(labelText);
-      };
-    });
-
+  $searchInput.on('blur', function() {
+    if ($.trim($(this).val() === '')) {
+      $(this).addClass('hint').val(labelText);
+    }
+  });
 }
 
 $(document).ready(function() {
