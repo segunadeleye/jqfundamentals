2016e972787295defd50270c09e5bb7617fe6746
Oluwasegun Adeleye <oluwasegun@dealdey.com>
2015-01-22 12:35:31 +0100

    Exercise 4 -- Create an input hint and Add tabbed navigation.

--- exercises/Ex4/inputHint.js
+++ exercises/Ex4/inputHint.js
@@ -1,39 +1,46 @@
 
+function InputHint() {
+}
+
+InputHint.prototype.createHint = function() {
+  
+  // 1. Set the value of the search input to the text of the label element
+
+    var $searchInput = $('input[name=q]');
+
+    var labelText = $('label').text();
+
+    $searchInput.val(labelText);
+
+
+  // 2. Add a class of "hint" to the search input
+
+    $searchInput.addClass('hint');
+
+
+  // 3. Remove the label element
+
+    $('label').remove();
+
+
+  // 4. Bind a focus event to the search input that removes the hint text and the "hint" class
+
+    $searchInput.on('focus', function() {
+      $(this).removeClass('hint').val('');
+    });
+
+
+  // 5. Bind a blur event to the search input that restores the hint text and "hint" class if no search text was entered
+
+    $searchInput.on('blur', function() {
+      if ($.trim($(this).val() === '')) {
+        $(this).addClass('hint').val(labelText);
+      };
+    });
+
+}
+
 $(document).ready(function() {
-
-// 1. Set the value of the search input to the text of the label element
-
-  $searchInput = $('input[name=q]');
-
-  labelText = $('label').text();
-
-  $searchInput.val(labelText);
-
-
-
-// 2. Add a class of "hint" to the search input
-
-  $searchInput.addClass('hint');
-
-
-// 3. Remove the label element
-
-  $('label').remove();
-
-
-// 4. Bind a focus event to the search input that removes the hint text and the "hint" class
-
-  $searchInput.on('focus', function() {
-    $(this).removeClass('hint').val('');
-  });
-
-
-// 5. Bind a blur event to the search input that restores the hint text and "hint" class if no search text was entered
-
-  $searchInput.on('blur', function() {
-    if ($.trim($(this).val() === '')) {
-      $(this).addClass('hint').val(labelText);
-    };
-  });
-
+  var inputHint = new InputHint();
+  inputHint.createHint();
 });
