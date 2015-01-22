b986c1bac7476cba9030bea3bc44df8f6cf2f096
Oluwasegun Adeleye <oluwasegun@dealdey.com>
2015-01-22 12:40:19 +0100

    Add tabbed navigation.

--- exercises/Ex4/tabs.js
+++ exercises/Ex4/tabs.js
@@ -1,49 +1,67 @@
 
-$(document).ready(function() {
+function Tabs() {
+}
 
-// 1. Hide all of the modules.
+Tabs.prototype.showContent = function() {
   
-  var $modules = $('.module')
-  $modules.hide();
-
-// 2. Create an unordered list element before the first module.
-
-  var $list = $('<ul></ul>');
-  $list.insertBefore($modules.first());
-
-// 3. Iterate over the modules using $.fn.each. For each module, use the text of the h2 element as the text for a list item that you add to the unordered list element.
-
-  $modules.each(function() {
-    var h2Text = $(this).children('h2').text();
-    var $listItem = $('<li>' + h2Text + '</li>');
-
-    $listItem.appendTo($list);
-  });
-
-// 4. Bind a click event to the list item that:
-
-    // a. Shows the related module, and hides any other modules
-
-    // b. Adds a class of "current" to the clicked list item
-
-    // c. Removes the class "current" from the other list item
+  // 1. Hide all of the modules.
     
-      $listItem = $list.children();
-      $listItem.on('click', function() {
-        var listItemIndex = $(this).index();
+    var $modules = $('.module')
+    $modules.hide();
 
-        $(this).addClass('current')
-                .siblings('li')
-                .removeClass('current');
+  // 2. Create an unordered list element before the first module.
 
-        $modules.eq(listItemIndex)
-                .show()
-                .siblings('.module')
-                .hide();
-      });
+    var $list = $('<ul></ul>');
+    $list.insertBefore($modules.first());
 
-// 5. Finally, show the first tab.
+  // 3. Iterate over the modules using $.fn.each. For each module, use the text of the h2 element as the text for a list item that you add to the unordered list element.
 
-    $modules.eq(0).show();
+    $modules.each(function() {
+      var h2Text = $(this).children('h2').text();
+      var $listItem = $('<li />').text(h2Text);
 
+      $listItem
+        .css('cursor', 'pointer')
+        .appendTo($list);
+    });
+
+  // 4. Bind a click event to the list item that:
+
+      // a. Shows the related module, and hides any other modules
+
+      // b. Adds a class of "current" to the clicked list item
+
+      // c. Removes the class "current" from the other list item
+      
+        $listItem = $list.children();
+        $listItem.on('click', function() {
+          var listItemIndex = $(this).index();
+
+          $(this)
+            .addClass('current')
+            .siblings('li')
+            .removeClass('current');
+
+          $modules
+            .eq(listItemIndex)
+            .show()
+            .siblings('.module')
+            .hide();
+        });
+
+  // 5. Finally, show the first tab.
+      
+      $listItem
+        .eq(0)
+        .addClass('current');
+
+      $modules
+        .eq(0)
+        .show();
+}
+  
+
+$(document).ready(function() {
+  var tabs = new Tabs();
+  tabs.showContent();
 });
