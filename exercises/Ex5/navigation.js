0cbf7eb562e9f80f85485075c1e1a263e9f38f7f
Oluwasegun Adeleye <oluwasegun@dealdey.com>
2015-01-22 12:48:34 +0100

    5.2 -- Create Drop down menus.

--- exercises/Ex5/navigation.js
+++ exercises/Ex5/navigation.js
@@ -1,16 +1,29 @@
 
-$(document).ready(function() {
+function Navigation() {
+}
 
+Navigation.prototype.showDropDown = function() {
+  
   $navTab = $('#nav li');
 
   $navTab.hover(
     function() {
-      $(this).addClass('hover')
-              .find('ul').show();
+      $(this)
+        .addClass('hover')
+        .find('ul')
+        .show();
     },
+    
     function() {
-      $(this).removeClass('hover')
-              .find('ul').hide();
+      $(this)
+        .removeClass('hover')
+        .find('ul')
+        .hide();
     }
   );
+}
+
+$(document).ready(function() {
+  var navigation = new Navigation;
+  navigation.showDropDown();
 });
