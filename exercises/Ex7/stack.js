0d19fefe90d695b62a00d6ce95c00527016ca928
Oluwasegun Adeleye <oluwasegun@dealdey.com>
2015-01-24 17:25:30 +0100

    .delegate() / .on() exercise.

---
+++ exercises/Ex7/stack.js
@@ -0,0 +1,36 @@
+function ItemStack() {
+  this.itemCount = 0;
+}
+
+ItemStack.prototype.addItem = function() {
+  
+  that = this;
+  $('#add').click(function() {
+    $('<div />')
+      .text(++that.itemCount)
+      .appendTo('#container');
+  });
+
+  this.hightlightItem();
+  this.removeItem();
+}
+
+ItemStack.prototype.hightlightItem = function() {
+
+  $('#container').delegate('div', 'click', function() {
+    $(this).css('backgroundColor', 'yellow');
+  });
+}
+
+ItemStack.prototype.removeItem = function() {
+
+  $('#container').on('click', 'div:last', function() {
+    $(this).remove();
+    --that.itemCount;
+  });
+}
+
+$(document).ready(function() {
+  var stack = new ItemStack();
+  stack.addItem();
+});
