268e6261eeb6e669af0cd5abc2c9bfeabd7edb92
Oluwasegun Adeleye <oluwasegun@dealdey.com>
2015-02-02 16:43:22 +0100

    Changes made.

--- exercises/Ex2/sandbox.js
+++ exercises/Ex2/sandbox.js
@@ -12,9 +12,9 @@ SandBox.prototype.selecting = function() {
 
   // 2. Come up with three selectors that you could use to get the third item in the #myList unordered list. Which is the best to use? Why?
 
-    $selector1 = $('#myListItem');
+    $selector1 = $('#myList').children()[2];
 
-    $selector2 = $('li#myListItem');
+    $selector2 = $('#myList').find('li:nth-child(3)');
 
     $selector3 = $('#myList li').eq(2);
 
@@ -68,9 +68,8 @@ SandBox.prototype.traversing = function() {
   // 4. Select the select element inside #specials; traverse your way to the submit button.
 
     $('#specials select')
-      .parent()
-      .next()
-      .children();
+      .closest('ul')
+      .find('input[type=submit]');
 
   // 5. Select the first list item in the #slideshow element; add the class “current” to it, and then add a class of “disabled” to its sibling elements
 
@@ -98,15 +97,15 @@ SandBox.prototype.manipulating = function() {
   // 3. Add another h2 and another paragraph to the last div.module
 
     $moduleClass2 = $('div.module:last');
-    $moduleClass2.append($('<h2 />').text('New Header'));
-    $moduleClass2.append($('<p />').text('New paragraph'));
+    $moduleClass2.append($('<h2 />').text('New Header'), $('<p />').text('New paragraph'));
 
   // 4. Add another option to the select element; give the option the value "Wednesday"
 
+    $tuesday = $('option[value=tuesday]');
     $newOption = $('<option />')
                     .attr('value', 'wednesday')
                     .text('Wednesday');
-    $('select').append($newOption);
+    $newOption.insertAfter($tuesday);
 
   // 5. Add a new div.module to the page after the last one; put a copy of one of the existing images inside of it.
 
