979ab578373f06ffcb35e74e4f55e7e1ea89ba51
Oluwasegun Adeleye <oluwasegun@dealdey.com>
2015-02-04 09:27:01 +0100

    Reduce DOM hits.

--- exercises/Ex5/blog.js
+++ exercises/Ex5/blog.js
@@ -1,22 +1,20 @@
-
 function Blog() {
 }
 
 Blog.prototype.slide = function() {
-  
+
   $h3 = $('#blog').find('h3');
 
   $h3.click(function(event) {
-    
-    event.preventDefault();
-    
-    $p = $(this).siblings('p');
-    $p.slideToggle();
 
-    $(this)
-      .parent()
-      .siblings()
-      .find('p')
+    event.preventDefault();
+
+    $p = $(this)
+          .next()
+          .slideToggle();
+
+    $('.excerpt')
+      .not($p)
       .slideUp();
   });
 }
